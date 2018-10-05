9a7f87bbe451015aedf6284f39f457bf8715525d
shellever <shellever@163.com>
2018-10-05 09:11:59 +0800

    automatic yes to prompts

--- quokka-vim.sh
+++ quokka-vim.sh
@@ -45,7 +45,8 @@ function vim_build_from_source()
     test "$vim_version" = "8.1" && return 0
 
     # 1. install all the prerequisite libraries
-    sudo apt-get install libncurses5-dev libgnome2-dev libgnomeui-dev \
+    sudo apt-get update
+    sudo apt-get install -y libncurses5-dev libgnome2-dev libgnomeui-dev \
         libgtk2.0-dev libatk1.0-dev libbonoboui2-dev \
         libcairo2-dev libx11-dev libxpm-dev libxt-dev python-dev \
         python3-dev ruby-dev lua5.1 liblua5.1-dev libperl-dev git
