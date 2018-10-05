1c8bd3b6c960cc8f8b0d2743100fb98e838dc331
shellever <shellever@163.com>
2018-10-05 18:31:58 +0800

    specify prefix for vim install

--- quokka-vim.sh
+++ quokka-vim.sh
@@ -61,14 +61,15 @@ function vim_build_from_source()
     git clone https://github.com/vim/vim.git
     cd vim
     make distclean
+
+    # Note for Ubuntu users: You can only use Python 2 or Python 3.
+    # --enable-python3interp=yes \
+    # --with-python3-config-dir=/usr/lib/python3.5/config \
     ./configure --with-features=huge \
         --enable-multibyte \
         --enable-rubyinterp=yes \
         --enable-pythoninterp=yes \
-        --with-python-config-dir=/usr/lib/python2.7/config \
-        # Note for Ubuntu users: You can only use Python 2 or Python 3.
-        # --enable-python3interp=yes \
-        # --with-python3-config-dir=/usr/lib/python3.5/config \
+        --with-python-config-dir=/usr/lib/python2.7/config-x86_64-linux-gnu \
         --enable-perlinterp=yes \
         --enable-luainterp=yes \
         --enable-gui=gtk2 \
